496c9d5dbbf3c87fd7aebc1d8d3aea3ba8c8b092
Marlon Marshall <marlon.subtil@rinf.tech>
2025-04-14 10:04:36 +0300

    Enhance guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,31 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io::stdin;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.");
+        }
+        Guess { value }
+    }
+    pub fn value(self: &Guess) -> i32 {
+        self.value
+    }
+    pub fn cmp(self: &Guess, secret_number: &i32) -> Ordering {
+        if self.value < *secret_number {
+            Ordering::Less
+        } else if self.value > *secret_number {
+            Ordering::Greater
+        } else {
+            Ordering::Equal
+        }
+    }
+}
+
 fn main() {
     let _secret_number = rand::thread_rng().gen_range(1..=100);
 
@@ -15,12 +40,12 @@ fn main() {
             Err(_) => continue,
         };
 
-        let guess: u32 = match guess_str.trim().parse() {
-            Ok(element) => element,
+        let guess: Guess = match guess_str.trim().parse() {
+            Ok(element) => Guess::new(element),
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
+        println!("You guessed: {}", guess.value);
 
         match guess.cmp(&_secret_number) {
             Ordering::Less => println!("Too small!"),
